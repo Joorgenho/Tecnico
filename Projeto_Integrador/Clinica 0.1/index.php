29848229a22840cf76b55a9c00699ea107d927f6
Joorgenho <50343325+Joorgenho@users.noreply.github.com>
2021-03-12 11:01:38 -0300

    Add files via upload

---
+++ Projeto_Integrador/Clinica 0.1/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+        <meta charset="UTF-8">
+        <title>Clínica Odontológica</title>
+    </head>
+    <body>
+        
+        <?php
+            require_once 'inicial.php';
+        ?>
+        
+        <br> <br> <br> <br> 
+        
+        <h1 align="center">Seja bem vindo a Clínica Odontológica! </h1>
+        
+        <a href="tipoConsulta.php">
+            <h1 align="center">Conheca nossos tipos de consultas </h1>;
+            
+        <?php
+            
+        ?>
+    </body>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+</html>
---
+++ Projeto_Integrador/Clinica 0.1/resultadoUsuario.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+         <?php
+        echo '<a href="sair.php"><button>Sair</button></a>';
+        ?>
+        <title>Editar/Excluir Usuário</title>
+    </head>
+    <body>
+    <table border="1">
+            <tr>
+                <th>Código</th>
+                <th>Foto</th>
+                <th>Nome do Cliente</th>
+                <th>CPF</th>
+                <th>Telefone</th>
+                <th>E-mail</th>
+                <th>Cidade</th>
+                <th>Sexo</th>
+                <th>Editar</th>
+                <th>Excluir</th>
+            </tr>
+            
+            <?php
+                    foreach ($lista as $cli){
+                        echo '<tr> ';
+                        echo '   <td>'.$cli->getId().'</td>';
+                        echo '   <td><img src="fotos_clientes/'.$cli->getFoto().'" width="30px" /></td>';
+                        echo '   <td>'.$cli->getNome().'</td>';
+                        echo '   <td>'.$cli->getCpf().'</td>';
+                        echo '   <td>'.$cli->getTelefone().'</td>';
+                        echo '   <td>'.$cli->getEmail().'</td>';
+                        echo '   <td>'.$cli->getCidade()->getNome().'</td>';
+                        if( $cli->getSexo() == "f" )
+                            echo '   <td>Feminino</td>';
+                        else
+                            echo '   <td>Masculino</td>';
+                        
+                        if( $cli->getFilhos() )
+                            echo '   <td>Sim</td>';
+                        else
+                            echo '   <td>Não</td>';
+                        
+                        echo '   <td><a href="frmCliente.php?editar&idCliente='.$cli->getId().'" ><button>Editar</button></a></td>';
+                        echo '   <td><a href="controller/salvarCliente.php?excluir&idCliente='.$cli->getId().'" ><button>Excluir</button></a></td>';
+                        echo '</tr>';
+                        
+                    }
+            ?>
+            
+        </table>
+        
+        <?php
+        
+            
+            
+        ?>
+        
+    </body>
+</html>
